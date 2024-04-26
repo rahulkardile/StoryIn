260d6661b0243b83f5ffce451c529af0764002d2
Rahul kardile <rahulkardile321@gmail.com>
2024-04-26 13:14:45 +0530

    client update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 // http://44.201.189.101:3300/
+// http://localhost:3300/
+
 
 export default defineConfig({
   server:{
     proxy: {
       "/api": {
-        target:"http://localhost:3300/",
+        target:"http://44.201.189.101:3300/",
         secure: false
       }
     }
