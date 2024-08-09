9a11e0608e960f9c86bad1defaec2aeb860e5d89
Rahul kardile <rahulkardile321@gmail.com>
2024-08-09 09:59:41 +0530

    horizontal  scalling added to server

--- server/index.js
+++ server/index.js
@@ -10,47 +10,20 @@ import razorpay from 'razorpay'
 import helmet from "helmet"
 import rateLimit from "express-rate-limit"
 import { S3Client } from '@aws-sdk/client-s3';
+import cluster from 'cluster';
+import os from "os";
 
 import User from "./Routes/UserRoutes.js"
 import OrederRoute from "./Routes/OrderRoute.js"
 import AudioRoute from "./Routes/AudioRoute.js"
 import FevRoute from "./Routes/FevRoute.js"
 
-const app = express();
-
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-    message: {
-        message: "Too many requests, please try again later.",
-        success: true,
-    }
-})
+const totalCPUs = os.cpus().length;
 
 dotenv.config();
 const PORT = process.env.PORT || 3300
 const MONGO_URL = process.env.MONGOURL;
 const MONGO_CLOUD_URL = process.env.MONGOURL_CLOUD;
-
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3300' || "*",
-    optionsSuccessStatus: 200
-}));
-
-app.use(helmet());
-app.use(limiter);
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-
-const __dirname = path.resolve();
-
-try {
-    mongoose.connect(MONGO_CLOUD_URL)
-        .then(() => console.log('Database is connected'))
-} catch (error) {
-    console.log('Database is error ' + error);
-}
 
 export const instance = new razorpay({
     key_id: process.env.KEYID,
@@ -65,93 +38,142 @@ export const s3Clinet = new S3Client({
     }
 })
 
-let cout = 1;
+if (cluster.isPrimary) {
+    console.log(`Total cpu's are ${totalCPUs} . . . \n`);
 
-app.get("/api/stream", async (req, res, next) => {
-    try {
-
-        const filePath = req.query.path;
-
-        // Check if file exists
-        if (!fs.existsSync(filePath)) {
-            return res.status(404).json({
-                "statusCode": 404,
-                "message": "File not found"
-            })
-        }
-
-        const stat = fs.statSync(filePath);
-        const fileSize = stat.size;
-
-        // Get requested byte range from headers
-        const range = req.headers.range;
-
-        let start = 0;
-        let end = fileSize - 1; //default entire file
-
-        if (range) {
-            const parts = range.replace(/bytes=/, '').split('-');
-            start = parseInt(parts[0], 10) || 0;
-            end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-        }
-
-        res.writeHead(200, {
-            'Content-Type': 'audio/mpeg',
-            'Content-Length': fileSize,
-            'accept-ranges': 'bytes',
-            'content-range': `bytes ${start}-${end}/${fileSize}`
-        })
-
-        const stream = fs.createReadStream(filePath);
-        stream.on('error', (err) => {
-            console.error(err)
-        })
-
-        stream.pipe(res);
-
-    } catch (error) {
-        next(error)
+    for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
     }
-})
 
-app.use("/api/uploads", express.static("uploads"))
-app.use("/api/user/", User)
-app.use("/api/audio-book/", AudioRoute)
-app.use("/api/order/", OrederRoute)
-app.use("/api/fev/", FevRoute)
+    cluster.on("exit", (Worker, code, single) => {
+        console.log(`Worker ${process.pid} is died`);
+        cluster.fork();
+    })
+} else {
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+    const app = express();
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100,
+        message: {
+            message: "Too many requests, please try again later.",
+            success: true,
+        }
+    })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-})
+    app.use(cors({
+        origin: process.env.CORS_ORIGIN || 'http://localhost:3300' || "*",
+        optionsSuccessStatus: 200
+    }));
 
-// app.get("*", (req, res, next)=>{
-//     try {
-//         res.status(404).json({
-//             success: true,
-//             message: "Route Not Found!"
-//         })
-//     } catch (error) {
-//         next(errorHandler(error))
-//     }
-// })
+    app.use(helmet());
+    app.use(limiter);
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }))
 
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 400;
+    try {
+        mongoose.connect(MONGO_CLOUD_URL);
+    } catch (error) {
+        console.log('Database is error ' + error);
+    }
 
-    if (err.code === '11000') return err.message = "user already exist!"
-    let message = err.message || 'Internal Server Error';
+    const __dirname = path.resolve();
 
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
+    let cout = 1;
+
+    app.get("/api/stream", async (req, res, next) => {
+        try {
+
+            const filePath = req.query.path;
+
+            // Check if file exists
+            if (!fs.existsSync(filePath)) {
+                return res.status(404).json({
+                    "statusCode": 404,
+                    "message": "File not found"
+                })
+            }
+
+            const stat = fs.statSync(filePath);
+            const fileSize = stat.size;
+
+            // Get requested byte range from headers
+            const range = req.headers.range;
+
+            let start = 0;
+            let end = fileSize - 1; //default entire file
+
+            if (range) {
+                const parts = range.replace(/bytes=/, '').split('-');
+                start = parseInt(parts[0], 10) || 0;
+                end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'audio/mpeg',
+                'Content-Length': fileSize,
+                'accept-ranges': 'bytes',
+                'content-range': `bytes ${start}-${end}/${fileSize}`
+            })
+
+            const stream = fs.createReadStream(filePath);
+            stream.on('error', (err) => {
+                console.error(err)
+            })
+
+            stream.pipe(res);
+
+        } catch (error) {
+            next(error)
+        }
+    })
+
+    app.use("/api/uploads", express.static("uploads"))
+    app.use("/api/user/", User)
+    app.use("/api/audio-book/", AudioRoute)
+    app.use("/api/order/", OrederRoute)
+    app.use("/api/fev/", FevRoute)
+    app.get("/pid", (req, res) => {
+        res.send({
+            message: `pid is ${process.pid}`,
+            success: true
+        })
+    })
+
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    })
+
+    // app.get("*", (req, res, next)=>{
+    //     try {
+    //         res.status(404).json({
+    //             success: true,
+    //             message: "Route Not Found!"
+    //         })
+    //     } catch (error) {
+    //         next(errorHandler(error))
+    //     }
+    // })
+
+    app.use((err, req, res, next) => {
+        const statusCode = err.statusCode || 400;
+
+        if (err.code === '11000') return err.message = "user already exist!"
+        let message = err.message || 'Internal Server Error';
+
+        return res.status(statusCode).json({
+            success: false,
+            statusCode,
+            message,
+        });
     });
-});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT} . . .`);
-})
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT} . . .`);
+    })
 
-// export const handler = serverless(app);
+    // export const handler = serverless(app);
+}
