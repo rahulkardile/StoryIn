fe76e54028fde39b34ab7c941e9cfbda43143c15
Rahul kardile <rahulkardile321@gmail.com>
2024-08-09 15:20:01 +0530

    have added

--- server/index.js
+++ server/index.js
@@ -141,10 +141,10 @@ if (cluster.isPrimary) {
         })
     })
 
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, 'dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     })
 
     // app.get("*", (req, res, next)=>{
